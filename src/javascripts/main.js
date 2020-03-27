056fe85349951349c151735920c45790eb2c9a19
David Everett <davideverett1989@gmail.com>
2020-03-27 01:02:52 -0500

    Authentication working just fine.

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -6,11 +6,15 @@ import apiKeys from './helpers/apiKeys.json';
 
 import home from './components/home/home';
 import auth from './components/auth/auth';
+import authData from './helpers/data/authData';
+import logout from './components/logout/logout';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   home.createHomeSection();
   auth.createSignInButton();
+  authData.checkLoginStatus();
+  logout.logoutEvent();
 };
 
 init();
