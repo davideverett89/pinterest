f9b8ebf35149b42595ee23171efd0b5938a2e83f
David Everett <davideverett1989@gmail.com>
2020-04-01 22:06:40 -0500

    Deleting pins and boards at the same time.

--- src/javascripts/components/boards/boards.js
+++ src/javascripts/components/boards/boards.js
@@ -4,6 +4,7 @@ import 'firebase/auth';
 import createBoards from '../createBoards/createBoards';
 import utils from '../../helpers/utils';
 import boardData from '../../helpers/data/boardData';
+import pinData from '../../helpers/data/pinData';
 
 import './boards.scss';
 
@@ -11,6 +12,13 @@ const removeBoard = (e) => {
   const boardId = e.target.closest('.card').id;
   boardData.deleteBoard(boardId)
     .then(() => {
+      pinData.getPinsByBoardId(boardId)
+        .then((pins) => {
+          pins.forEach((pin) => {
+            const pinId = pin.id;
+            pinData.deletePinsByPinId(pinId);
+          });
+        });
       // eslint-disable-next-line no-use-before-define
       printBoards();
       utils.printToDom('singleView', '');
