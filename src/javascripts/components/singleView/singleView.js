31288507c545c85df404d0a7ec67e1781020d22b
David Everett <davideverett1989@gmail.com>
2020-04-01 21:43:14 -0500

    Deleting pins.

--- src/javascripts/components/singleView/singleView.js
+++ src/javascripts/components/singleView/singleView.js
@@ -3,21 +3,46 @@ import utils from '../../helpers/utils';
 
 import './singleView.scss';
 
+const removePin = (e) => {
+  const pinId = e.target.closest('.pin').id;
+  const boardId = e.target.closest('.pin-container').id;
+  pinData.deletePinsByPinId(pinId)
+    .then(() => {
+      pinData.getPinsByBoardId(boardId)
+        .then((pins) => {
+          let domString = '';
+          pins.forEach((pin) => {
+            domString += `<div class="pin-container" id="${boardId}">`;
+            domString += `<div class="pin" id="${pin.id}">`;
+            domString += '<button class="delete-pin"><i class="far fa-times-circle"></i></button>';
+            domString += `<img class="pin-image" src="${pin.imageUrl}" alt="${pin.id}" />`;
+            domString += '</div>';
+            domString += '</div>';
+          });
+          utils.printToDom('singleView', domString);
+          $('#single-board-modal').modal('show');
+          $('body').on('click', '.delete-pin', removePin);
+        });
+    })
+    .catch((err) => console.error('Something is wrong', err));
+};
+
 const buildSingleView = (e) => {
   const boardId = e.target.closest('.card').id;
-  console.error(boardId);
   pinData.getPinsByBoardId(boardId)
     .then((pins) => {
       let domString = '';
       pins.forEach((pin) => {
-        domString += `<div id="${pin.id}">`;
+        domString += `<div class="pin-container" id="${boardId}">`;
+        domString += `<div class="pin" id="${pin.id}">`;
         domString += '<button class="delete-pin"><i class="far fa-times-circle"></i></button>';
         domString += `<img class="pin-image" src="${pin.imageUrl}" alt="${pin.id}" />`;
         domString += '</div>';
-        console.error(pin.imageUrl);
+        domString += '</div>';
       });
       utils.printToDom('singleView', domString);
       $('#single-board-modal').modal('show');
+      $('body').on('click', '.delete-pin', removePin);
     })
     .catch((err) => console.error('You fucked up', err));
 };
