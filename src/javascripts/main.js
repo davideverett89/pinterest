9cd2489dade4ed49770dc65fa72475a43a7fee51
David Everett <davideverett1989@gmail.com>
2020-04-06 20:39:09 -0500

    Adding boards.

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -9,6 +9,7 @@ import auth from './components/auth/auth';
 import authData from './helpers/data/authData';
 import logout from './components/logout/logout';
 import singleView from './components/singleView/singleView';
+import addBoard from './components/addBoard/addBoard';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
@@ -16,6 +17,7 @@ const init = () => {
   auth.createSignInButton();
   authData.checkLoginStatus();
   logout.logoutEvent();
+  $('#add-board-button').click(addBoard.buildAddBoardForm);
   $('body').on('click', '.single-view', singleView.buildSingleView);
   $('#exit-single-view').click(singleView.closeSingleView);
 };
