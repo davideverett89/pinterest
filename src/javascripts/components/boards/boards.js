96f0a98c5b302253f095de67e0aaac76d81438b4
David Everett <davideverett1989@gmail.com>
2020-04-12 18:11:37 -0500

    Lookin' pretty dang good.

--- src/javascripts/components/boards/boards.js
+++ src/javascripts/components/boards/boards.js
@@ -14,14 +14,21 @@ const printBoards = () => {
   const myUid = firebase.auth().currentUser.uid;
   boardData.getBoardsByUid(myUid)
     .then((boards) => {
-      let domString = '';
-      domString += '<h1 id="board-header" class="p-3 display-4">My Boards</h1>';
-      domString += '<div id="board-container" class="p-3 d-flex flex-wrap">';
-      boards.forEach((board) => {
-        domString += createSingleBoard.boardMaker(board);
-      });
-      domString += '</div>';
-      utils.printToDom('boards', domString);
+      if (boards.length > 0) {
+        let domString = '';
+        domString += '<h1 id="board-header" class="p-3 display-4">My Boards</h1>';
+        domString += '<div id="board-container" class="p-3 d-flex flex-wrap">';
+        boards.forEach((board) => {
+          domString += createSingleBoard.boardMaker(board);
+        });
+        domString += '</div>';
+        utils.printToDom('boards', domString);
+        utils.printToDom('empty-page-message', '');
+      } else {
+        const domString = '<h4>Click the green button to add a board!  >>>>></h4>';
+        utils.printToDom('empty-page-message', domString);
+        utils.printToDom('boards', '');
+      }
     })
     .catch((err) => console.error('Print boards is not working', err));
 };
@@ -41,7 +48,7 @@ const addBoardEvent = (e) => {
       .then(() => {
         $('#new-board-form').trigger('reset');
         $('#add-board-modal').modal('hide');
-        printBoards(e.data);
+        printBoards();
       })
       .catch((err) => console.error('Could not add a new board', err));
   }
@@ -59,7 +66,7 @@ const removeBoard = (e) => {
           });
         });
       printBoards();
-      utils.printToDom('singleView', '');
+      utils.printToDom('single-view', '');
     })
     .catch((err) => console.error('Something is not right', err));
 };
